68ffd8a3fbefb4b22dcabe7e254d892132cc2dfe
Julio Cesar <cesar.everlastingtechnologydeveloper@outlook.com.br>
2023-09-17 13:47:24 -0300

    iniciação do projeto

---
+++ src/main/java/br/edu/infenet/projeto/appgestaoterrenofrutas/AppgestaoterrenofrutasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infenet.projeto.appgestaoterrenofrutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgestaoterrenofrutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgestaoterrenofrutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infenet/projeto/appgestaoterrenofrutas/AppgestaoterrenofrutasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infenet.projeto.appgestaoterrenofrutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgestaoterrenofrutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
